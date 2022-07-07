e7e6d26ab39a61d11e9b8e3e37a7a69ae8453711
Lucas <lucasbivarfonseca@gmail.com>
2022-07-07 20:05:13 -0300

    creating home

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -6,11 +6,11 @@ export const Home = (props) => {
   return(
     <Layout sidebar>
       
-      {/* <div style={{margin: "5rem", background: "#FFF"}} className='jumbotron text-center'>
+      <div style={{margin: "5rem", background: "#FFF"}} className='jumbotron text-center'>
         <h1>Welcome to Admin Dashboard</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni aliquid voluptate numquam expedita vero, ipsam maiores distinctio in, repudiandae quam eligendi et reprehenderit, temporibus voluptates magnam ab? Perspiciatis quaerat consequuntur consectetur, cumque asperiores deleniti optio? Cumque nisi itaque aut fugiat!</p>
 
-      </div> */}
+      </div>
 
     </Layout>
   );
